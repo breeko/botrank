edfccbb27beaff4ff548e9554505a9892cfd8ba5
Bosko Blagojevic <bosko.blagojev@gmail.com>
2018-08-11 10:38:59 -0400

    initial

---
+++ botrank.py
@@ -0,0 +1,27 @@
+import csv
+from flask import Flask, render_template
+app = Flask(__name__)
+
+def read_csv(filename="bots.csv"):
+  rt = []
+  fh = open(filename, "r")
+  reader = csv.reader(fh, delimiter=",")
+  for row in reader:
+    print (row)
+    rank = {
+      "name": row[0],
+      "url": row[1],
+      "rank": row[2],
+      "karma": row[3],
+      "replies": row[4],
+      "read_time": row[5]
+    }
+    rt.append(rank)
+
+  return rt
+
+@app.route("/", methods=['GET'])
+def index():
+  bots = read_csv()
+  bots = sorted(bots, key=lambda k: k["rank"]) 
+  return render_template("index.html", bots=bots)
