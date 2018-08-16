1ba40dab17e46495247342730942ba0bdd6b8ea5
Bosko Blagojevic <bosko.blagojev@gmail.com>
2018-08-16 00:14:37 -0400

    page through bots 250 at a time

--- botrank.py
+++ botrank.py
@@ -2,6 +2,8 @@ import csv
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+ITEMS_PER_PAGE = 250
+
 def read_csv(filename="bots.csv"):
   rt = []
   fh = open(filename, "r")
@@ -21,22 +23,40 @@ def index():
 
   sort = request.args.get("sort", "rank")
   order = request.args.get("order")
+  page = request.args.get("page", "1")
 
   cols = [ "rank", "name", "score", "good-votes", "bad-votes" ]
 
   numeric_cols = {"rank", "score", "good-votes", "bad-votes"}
   reverse_cols = {"score", "good-votes", "bad-votes"}
 
-  if sort in cols:
-    if sort in reverse_cols:
-      reverse = True
-    else:
-      reverse = False
+  if sort not in cols:
+    sort = "rank"
 
-    if order:
-      reverse = not reverse
+  if sort in reverse_cols:
+    reverse = True
+  else:
+    reverse = False
 
-    bots = sorted(bots, key=lambda k: float(k[sort]) if sort in numeric_cols else k[sort], reverse=reverse)
+  if order:
+    reverse = not reverse
 
-  params = {"sort": sort, "order": order}
+  total = len(bots)
+  if total > int(page) * ITEMS_PER_PAGE:
+    more = True
+  else:
+    more = False
+
+  bots = sorted(bots, key=lambda k: float(k[sort]) if sort in numeric_cols else k[sort], reverse=reverse)
+
+  if page.isdigit() and int(page) > 0:
+    page = int(page)
+    page = page - 1
+    bots = bots[(page * ITEMS_PER_PAGE):((page * ITEMS_PER_PAGE) + ITEMS_PER_PAGE)]
+    page = page + 1
+  else:
+    page = 1
+
+  params = {"sort": sort, "order": order, "page": page, "total": total, "more": more}
+
   return render_template("index.html", bots=bots, params=params)
